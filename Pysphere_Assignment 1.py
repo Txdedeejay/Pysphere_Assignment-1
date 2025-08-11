12d75953699c6e65d57714d1abce3d94f518474e
Txdedeejay <mrmokebei@gmail.com>
2025-08-11 19:30:14 +0100

    Add files via upload

---
+++ Pysphere_Assignment 1.py
@@ -0,0 +1,7 @@
+#Write a Python script to calculate Voltage(V=I*R)
+#IT SHOULD HAVE A USER INPUT
+i = int(input("Input Current : "))
+r = int(input("Input Resistance : "))
+v = i*r
+#I*R=V as output
+print("Voltage is : ", v)
